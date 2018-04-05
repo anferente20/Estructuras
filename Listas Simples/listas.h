7e59a2f92a015f3be124ee6ae92afb51a2c734de
anferente20 <34009575+anferente20@users.noreply.github.com>
2018-04-05 15:21:41 -0500

    Add files via upload

---
+++ Listas Simples/listas.h
@@ -0,0 +1,115 @@
+template<class T>
+struct Nodo{
+	T x;
+	Nodo<T> *sig;
+}
+class Lista{
+	T *cab;
+	T *fin;
+	int tam;
+	
+	public:
+		Lista(){
+			cab=fin=NULL;
+			tam=0;
+		}
+		bool estaVacia(){
+			if(tam ==0)
+				return true;
+			else
+				return false;
+		}
+		
+		void anadirFin(T a){
+			Nodo<T> b = new Nodo<T>;
+			b->x = a;
+			b-> sig = NULL;
+			if(estaVacia()){
+				cab = b;
+				fin = b;
+			}
+			else{
+				fin->sig = b;
+				fin = b;
+			}
+			tam++;
+		}
+		
+		void anadirCabeza(T a){
+			Nodo<T> b = new Nodo<T>;
+			b->x = a;
+			b-> sig = NULL;
+			if(estaVacia()){
+				cab = b;
+				fin = b;
+			}
+			else{
+				b->sig = cab;
+				cab=b
+			}
+			tam++;
+		}
+		
+		void anadirPos(T a, int Pos){
+			Nodo<T> b = new Nodo<T>;
+			b->x = a;
+			b-> sig = NULL;
+			if(estaVacia())
+				anadirFin(T a);
+			if(Pos>tam)
+				anadirCabeza(T a);
+			else{
+				int contador = 1;
+				Nodo<T> aux = new Nodo<T>;
+				aux = cab;
+				while(contador<Pos-1){
+					if(contador == Pos){
+						b->sig = aux->sig;
+						aux->sig = b;
+ 					}
+					else{
+						aux = aux->sig;
+						contador++;
+					}
+				}
+			}
+		}
+		
+		
+		void modificar(T a,int Pos){
+			if(estaVacia())
+				anadirFin(T a);
+			if(Pos>tam)
+				anadirCabeza(T a);
+			else{
+				int contador = 1;
+				Nodo<T> aux = new Nodo<T>;
+				aux = cab;
+				while(contador<Pos){
+					if(contador == Pos){
+						aux->x =a;
+ 					}
+					else{
+						aux = aux->sig;
+						contador++;
+					}
+				}
+			}	
+		}
+		
+		void eliminarFinal(){
+			int contador = 1;
+			Nodo<T> aux = new Nodo<T>;
+			aux = cab;
+			while(contador<= tam-1){
+				if(contador == tam){
+					delete aux->sig;
+					aux->sig = NULL;
+				}
+				else{
+					aux = aux->sig;
+						contador++;
+				}
+			}
+		}
+};
